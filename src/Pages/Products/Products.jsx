31bf84a9de6c4faeb593ea79ed8c6ce10ee205dc
asif12018 <asif.request@gmail.com>
2024-08-17 13:19:54 +0600

    product page added

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import Hero from "../../components/Hero/Hero";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 
 const Products = () => {
+    const [products, setProducts] = useState([]);
+    
+    const {data:availableProducts, isLoading, refetch} = useQuery({
+        queryKey:['availableProducts'],
+        queryFn: async()=>{
+            const res = await axios.get('http://localhost:5000/allProducts')
+            return res.data;
+        }
+    })
+
+     if(isLoading){
+        return <div>Loading</div>
+     }
+     console.log(availableProducts);
     return (
         <div>
             <Hero></Hero>
--- src/main.jsx
+++ src/main.jsx
@@ -13,8 +13,14 @@ import App from './App';
 import AuthProvider from './provider/AuthProvider';
 import Register from './Pages/Register/Register';
 import Products from './Pages/Products/Products';
-
-
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 
 const router = createBrowserRouter([
@@ -44,8 +50,10 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+    <AuthProvider>
      <RouterProvider router={router} />
      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
